52d6ef7b3eb355da5e7e21fc5301d6ef041af58a
rajneeshyadav <yadavrajneesh481@gmail.com>
2023-02-25 21:56:59 +0530

    taking input in console

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var username = prompt("give me your name")
-alert("hii  " + username)
+const btn = document.querySelector("#btn");
+const txt_input = document.querySelector("#txt-input");
+console.log(txt_input)
+function clicked() {
+    console.log("clicked");
+    console.log("input", txt_input.value);
+}
+btn.addEventListener("click", clicked);
+
+
